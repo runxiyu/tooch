9edb92c9975193d0ad1ac071c125ea969333db69
Runxi Yu <me@runxiyu.org>
2024-06-10 08:00:00 +0800

    chromium-html-to-pdf: double-quote arguments

--- pdf-utils/chromium-html-to-pdf.sh
+++ pdf-utils/chromium-html-to-pdf.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-chromium-browser --headless --no-pdf-header-footer --print-to-pdf=p$(basename -s .svg $1).pdf $1
+chromium-browser --headless --no-pdf-header-footer --print-to-pdf="$(basename -s .svg "$1").pdf" "$1"
