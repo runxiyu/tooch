96dbd46d25494b0ba089865de42db399a91b44a9
Runxi Yu <me@runxiyu.org>
2024-06-22 08:00:00 +0800

    sjauth: Make indentation less stupid

--- sjauth.c
+++ sjauth.c
@@ -59,40 +59,28 @@ void login(const char *username, const char *password, char *response)
 	rc4(password, ts, rc4_output);
 
 	char postfields[512];
-	snprintf(postfields, sizeof(postfields),
-		 "opr=pwdLogin&userName=%s&pwd=%s&rc4Key=%s&auth_tag=%s&rememberPwd=1",
-		 username, rc4_output, ts, ts);
+	snprintf(postfields, sizeof(postfields), "opr=pwdLogin&userName=%s&pwd=%s&rc4Key=%s&auth_tag=%s&rememberPwd=1", username, rc4_output, ts, ts);
 
 	curl_global_init(CURL_GLOBAL_DEFAULT);
 	curl = curl_easy_init();
 	if (curl) {
-		curl_easy_setopt(curl, CURLOPT_URL,
-				 "http://sjauth.ykpaoschool.cn/ac_portal/login.php");
+		curl_easy_setopt(curl, CURLOPT_URL, "http://sjauth.ykpaoschool.cn/ac_portal/login.php");
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postfields);
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, wc);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, response);
 
 		struct curl_slist *headers = NULL;
-		headers =
-		    curl_slist_append(headers,
-				      "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:104.0) Gecko/20100101 Firefox/104.0");
-		headers =
-		    curl_slist_append(headers,
-				      "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
-		headers =
-		    curl_slist_append(headers,
-				      "Accept-Language: en-US.en;q=0.5");
-		headers =
-		    curl_slist_append(headers,
-				      "Content-Type: application/x-www-form-urlencoded; charset=UTF-8");
+		headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:104.0) Gecko/20100101 Firefox/104.0");
+		headers = curl_slist_append(headers, "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
+		headers = curl_slist_append(headers, "Accept-Language: en-US.en;q=0.5");
+		headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded; charset=UTF-8");
 		headers = curl_slist_append(headers, "Origin: null");
 		headers = curl_slist_append(headers, "Connection: keep-alive");
 		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
 		res = curl_easy_perform(curl);
 		if (res != CURLE_OK) {
-			fprintf(stderr, "curl_easy_perform() failed: %s\n",
-				curl_easy_strerror(res));
+			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
 		}
 
 		curl_slist_free_all(headers);
@@ -104,18 +92,14 @@ void login(const char *username, const char *password, char *response)
 int main(int argc, char *argv[])
 {
 	if (argc != 3) {
-		fprintf(stderr,
-			"%s: Invalid arguments. The first argument shall be your username, and the second shall be the name of the environment variable containing your password.\n",
-			argv[0]);
+		fprintf(stderr, "%s: Invalid arguments. The first argument shall be your username, and the second shall be the name of the environment variable containing your password.\n", argv[0]);
 		return 1;
 	}
 
 	const char *username = argv[1];
 	const char *password = getenv(argv[2]);
 	if (!password) {
-		fprintf(stderr,
-			"%s: Environment variable %s does not exist. You shall cause the variable identified by %s to contain your password.\n",
-			argv[0], argv[2], argv[2]);
+		fprintf(stderr, "%s: Environment variable %s does not exist. You shall cause the variable identified by %s to contain your password.\n", argv[0], argv[2], argv[2]);
 		return 2;
 	}
 
