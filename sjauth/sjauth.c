f7e9bd9856ed5266028cc983cb98f23fb71cc820
Runxi Yu <me@runxiyu.org>
2024-07-08 08:00:00 +0800

    sjauth: Update User-Agent

--- sjauth/sjauth.c
+++ sjauth/sjauth.c
@@ -67,7 +67,7 @@ int login(const char *username, const char *password)
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postfields);
 
 		struct curl_slist *headers = NULL;
-		headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:104.0) Gecko/20100101 Firefox/104.0");
+		headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0");
 		headers = curl_slist_append(headers, "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
 		headers = curl_slist_append(headers, "Accept-Language: en-US.en;q=0.5");
 		headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded; charset=UTF-8");
